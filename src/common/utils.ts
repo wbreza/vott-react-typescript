29b6468eb7c105bf969df398ea79abc3d9c33393
Tanner Barlow <tanner.barlow12@gmail.com>
2018-12-03 09:55:27 -0800

    utils test

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,5 @@
-export const randomIntInRange = (min, max) => {
+export function randomIntInRange(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
-};
+}
