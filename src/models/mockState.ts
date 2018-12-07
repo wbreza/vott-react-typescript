255574e785e98b1618c0138a36a26192dd6a2ab3
Tanner Barlow <tanner.barlow12@gmail.com>
2018-12-07 10:16:39 -0800

    initializing wrapper

--- src/models/mockState.ts
+++ src/models/mockState.ts
@@ -1,113 +1,112 @@
 import ProjectSettingsPage from "../react/components/pages/projectSettings/projectSettingsPage";
-import { IProject, IConnection, IExportFormat, ITag, IAsset, IApplicationState, IAppSettings, IAssetMetadata, ISize } from "../models/applicationState";
+import { IProject, IConnection, IExportFormat,
+    ITag, IAsset, IApplicationState, IAppSettings,
+    IAssetMetadata, ISize } from "../models/applicationState";
 import { IProjectService } from "../services/projectService";
 import IProjectActions from "../redux/actions/projectActions";
 import IConnectionActions, { loadConnection, deleteConnection } from "../redux/actions/connectionActions";
 
-// export const MockApplicationState : IApplicationState = {
+export class MockFactory {
+    public tags(): ITag[] {
+        return [
+            {
+                name: "Tag1",
+                color: "#FFFFFF",
+            },
+            {
+                name: "Tag2",
+                color: "#FFFF00",
+            },
+        ];
+    }
 
-// }
+    public connections(): IConnection[] {
+        return [
+            {
+                id: "connectionId",
+                name: "My source connection",
+                description: "This is my connection",
+                providerType: "azureBlobStorage",
+                providerOptions: {
+                    connectionString: "myconnectionstring",
+                    containerName: "container",
+                    createContainer: true,
+                },
+            },
+            {
+                id: "connectionId",
+                name: "My target connection",
+                description: "This is my connection",
+                providerType: "localFileSystemProxy",
+                providerOptions: {
+                    folderPath: "my path",
+                },
+            },
+        ];
+    }
 
-// export const MockAppSettings :IAppSettings = {
+    public exportFormat(): IExportFormat {
+        return {
+            id: "exportFormatId",
+            name: "exportFormatName",
+            providerType: "tensorflow",
+            providerOptions: {},
+        };
+    }
 
-// }
-export const MockProject: IProject = {
-    id: "id",
-    name: "Test Project",
-    description: "This is my project",
-    tags: [
-        {
-            name: "Tag1",
-            color: "#FFFFFF",
-        },
-        {
-            name: "Tag2",
-            color: "#FFFF00",
-        },
-    ],
-    sourceConnection: {
-        id: "connectionId",
-        name: "My source connection",
-        description: "This is my connection",
-        providerType: "azureBlobStorage",
-        providerOptions: {
-            connectionString: "myconnectionstring",
-            containerName: "container",
-            createContainer: true,
-        },
-    },
-    targetConnection: {
-        id: "connectionId",
-        name: "My target connection",
-        description: "This is my connection",
-        providerType: "localFileSystemProxy",
-        providerOptions: {
-            folderPath: "my path",
-        },
-    },
-    exportFormat: {
-        id: "exportFormatId",
-        name: "exportFormatName",
-        providerType: "tensorflow",
-        providerOptions: {},
-    },
-    autoSave: true,
-};
+    public project(): IProject {
+        const connections = this.connections();
+        return {
+            id: "id",
+            name: "Test Project",
+            description: "This is my project",
+            tags: this.tags(),
+            sourceConnection: connections[0],
+            targetConnection: connections[1],
+            exportFormat: this.exportFormat(),
+            autoSave: true,
+        };
+    }
 
-// export const MockTag: ITag = {
+    public projectService(): IProjectService {
+        return {
+            get: jest.fn((id: string) => Promise.resolve()),
+            getList: jest.fn(() => Promise.resolve()),
+            save: jest.fn((project: IProject) => Promise.resolve()),
+            delete: jest.fn((project: IProject) => Promise.resolve()),
+        };
+    }
 
-// }
+    public projectActions(): IProjectActions {
+        return {
+            loadProjects: jest.fn(() => Promise.resolve()),
+            loadProject: jest.fn((value: IProject | string) => Promise.resolve()),
+            saveProject: jest.fn((project: IProject) => Promise.resolve()),
+            deleteProject: jest.fn((project: IProject) => Promise.resolve()),
+            closeProject: jest.fn(() => Promise.resolve()),
+            loadAssets: jest.fn((project: IProject) => Promise.resolve()),
+            saveAsset: jest.fn((asset: IAsset) => Promise.resolve()),
+        };
+    }
 
-// export const MockConnection: IConnection = {
+    public connectionActions(): IConnectionActions {
+        return {
+            loadConnections: jest.fn(() => Promise.resolve()),
+            loadConnection: jest.fn((connectionId: string) => Promise.resolve()),
+            saveConnection: jest.fn((connectionId: string) => Promise.resolve()),
+            deleteConnection: jest.fn((connectionId: string) => Promise.resolve()),
+            closeConnection: jest.fn(() => Promise.resolve()),
+        };
+    }
 
-// }
-
-// export const MockExportFormat: IExportFormat = {
-
-// }
-
-// export const MockAsset: IAsset = {
-
-// }
-
-// export const MockAssetMetadata: IAssetMetadata = {
-
-// }
-
-// export const MockSize: ISize = {
-
-// }
-
-// export const MockRegion: IRegion = {
-
-// };
-
-export const MockProjectService: IProjectService = {
-    get: jest.fn((id: string) => Promise.resolve()),
-    getList: jest.fn(() => Promise.resolve()),
-    save: jest.fn((project: IProject) => Promise.resolve()),
-    delete: jest.fn((project: IProject) => Promise.resolve()),
-};
-
-export const MockProjectActions: IProjectActions = {
-    loadProjects: jest.fn(() => Promise.resolve()),
-    loadProject: jest.fn((value: IProject | string) => Promise.resolve()),
-    saveProject: jest.fn((project: IProject) => Promise.resolve()),
-    deleteProject: jest.fn((project: IProject) => Promise.resolve()),
-    closeProject: jest.fn(() => Promise.resolve()),
-    loadAssets: jest.fn((project: IProject) => Promise.resolve()),
-    saveAsset: jest.fn((asset: IAsset) => Promise.resolve()),
-};
-
-
-
-export const MockConnectionActions: IConnectionActions = {
-    loadConnections: jest.fn(() => Promise.resolve()),
-    loadConnection: jest.fn((connectionId: string) => Promise.resolve()),
-    saveConnection: jest.fn((connectionId: string) => Promise.resolve()),
-    deleteConnection: jest.fn((connectionId: string) => Promise.resolve()),
-    closeConnection: jest.fn(() => Promise.resolve()),
-};
-
-export const MockConnections: IConnection[] = [
-];
+    public match() {
+        return {
+            params: {
+                projectId: null,
+            },
+            isExact: null,
+            path: null,
+            url: null,
+        };
+    }
+}
--- src/react/components/pages/projectSettings/projectSettingsPage.test.tsx
+++ src/react/components/pages/projectSettings/projectSettingsPage.test.tsx
@@ -1,16 +1,12 @@
-import React from "react";
 import { mount } from "enzyme";
+import React from "react";
 import { Provider } from "react-redux";
-import createReduxStore from "../../../../redux/store/store";
-import initialState from "../../../../redux/store/initialState";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Link } from "react-router-dom";
-
+import { IApplicationState } from "../../../../models/applicationState";
+import { MockFactory } from "../../../../models/mockState";
+import initialState from "../../../../redux/store/initialState";
+import createReduxStore from "../../../../redux/store/store";
 import ProjectSettingsPage from "./projectSettingsPage";
-import { IApplicationState, IProject, IConnection, IExportFormat, ITag, IAsset } from "../../../../models/applicationState";
-import { IProjectService } from "../../../../services/projectService";
-import IProjectActions from "../../../../redux/actions/projectActions";
-import { MockProjectActions, MockProject, MockConnections, MockConnectionActions } from "../../../../models/mockState";
 
 describe("Project settings page", () => {
     let onChangeHandler: (value: any) => void;
@@ -18,28 +14,18 @@ describe("Project settings page", () => {
 
     const defaultState: IApplicationState = initialState;
     const store = createReduxStore(defaultState);
-
-    beforeEach(() => {
-        onChangeHandler = jest.fn();
-        wrapper = mount(
-            <Provider store={store}>
-                <Router>
-                    <ProjectSettingsPage
-                        project={MockProject}
-                        projectActions={MockProjectActions}
-                        connectionActions={MockConnectionActions}
-                        connections={MockConnections}
-                        history={null}
-                        location={null}
-                        match={null}/>,
-                </Router>
-            </Provider>,
-        );
-    });
+    const history: any = null;
+    const location: any = null;
+    const mockFactory = new MockFactory();
+    const match = mockFactory.match();
+    const project = mockFactory.project();
+    const projectActions = mockFactory.projectActions();
+    const connectionActions = mockFactory.connectionActions();
+    const connections = mockFactory.connections();
 
     describe("Empty project", () => {
         it("should have empty project name field", () => {
-            throw new Error("Not implemented");
+            expect(wrapper.find("#input#root_name").props().value).toBeUndefined();
         });
 
         it("should have empty source connection field", () => {
@@ -61,35 +47,54 @@ describe("Project settings page", () => {
         it("should have empty tags input", () => {
             throw new Error("Not implemented");
         });
-    
+
         it("should render new connection page on clicking Add Connection", () => {
-            throw new Error("Not implemented");    
+            throw new Error("Not implemented");
         });
-    
+
         it("should call onChange handler when typing into text boxes", () => {
             throw new Error("Not implemented");
         });
-    
+
         it("should call onChange handler when creating a new tag", () => {
             throw new Error("Not implemented");
         });
-    
+
         it("should add new tags to state", () => {
             throw new Error("Not implemented");
         });
-    
+
         it("should remove tags from state", () => {
             throw new Error("Not implemented");
         });
-    
+
         it("should re-order tags in state", () => {
             throw new Error("Not implemented");
         });
     });
 
     describe("Load project", () => {
+
+        beforeEach(() => {
+            onChangeHandler = jest.fn();
+
+            wrapper = mount(
+                <Provider store={store}>
+                    <Router>
+                        <ProjectSettingsPage
+                            project={project}
+                            projectActions={projectActions}
+                            connectionActions={connectionActions}
+                            connections={connections}
+                            history={history}
+                            location={location}
+                            match={match}/>
+                    </Router>
+                </Provider>,
+            );
+        });
         it("should load project name into field", () => {
-            throw new Error("Not implemented");
+            expect(wrapper.find("input#root_name").props().value).toBe(project.name);
         });
 
         it("should load source connection into field", () => {
@@ -99,11 +104,11 @@ describe("Project settings page", () => {
         it("should load target connection into field", () => {
             throw new Error("Not implemented");
         });
-        
+
         it("should load export format into field", () => {
             throw new Error("Not implemented");
         });
-        
+
         it("should load description into field", () => {
             throw new Error("Not implemented");
         });
@@ -111,5 +116,5 @@ describe("Project settings page", () => {
         it("should load tags into widget", () => {
             throw new Error("Not implemented");
         });
-    });    
+    });
 });
